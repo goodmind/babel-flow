5ab26b8a7319cdf6615d5615eb2dd069d30f677b
andretshurotshka <andwebar@gmail.com>
2019-03-30 20:05:53 +0500

    Use flow-bin as file

--- src/ast.js
+++ src/ast.js
@@ -6,26 +6,21 @@ import * as t from "@babel/types";
 import * as parser from "@babel/parser";
 import traverse from "@babel/traverse";
 import template from "@babel/template";
-
-function flowBinVersion(): string | null {
-  try {
-    //$FlowFixMe
-    const version = require("flow-bin/package.json").version;
-    return version;
-  } catch (e) {
-    return null;
-  }
-}
-
-const flowVersion = flowBinVersion();
+import flow from "flow-bin";
 
 async function getTypeAtPos(
   { line = 0, column = 0 } = { line: 0, column: 0 },
   filename: string
 ) {
-  const command = `npx flow type-at-pos --expand-type-aliases --json ${filename} ${line} ${column +
-    1}`;
-  const result = await execa.shell(command);
+  const args = [
+    "type-at-pos",
+    "--expand-type-aliases",
+    "--json",
+    filename,
+    line,
+    column + 1
+  ];
+  const result = await execa(flow, args);
   return JSON.parse(result.stdout);
 }
 
@@ -33,21 +28,33 @@ function getTypeAtPosSync(
   { line = 0, column = 0 } = { line: 0, column: 0 },
   filename: string
 ) {
-  const command = `npx flow type-at-pos --expand-type-aliases --json ${filename} ${line} ${column +
-    1}`;
-  return JSON.parse(execa.shellSync(command).stdout);
+  const args = [
+    "type-at-pos",
+    "--expand-type-aliases",
+    "--json",
+    filename,
+    line,
+    column + 1
+  ];
+  return JSON.parse(execa.sync(flow, args).stdout);
 }
 
 function getTypeAtPosExpandedSync(
   { line = 0, column = 0 } = { line: 0, column: 0 },
   filename: string
 ) {
-  const command = `npx flow type-at-pos --expand-type-aliases --expand-json-output ${filename} ${line} ${column +
-    1}`;
-  return JSON.parse(execa.shellSync(command).stdout);
+  const args = [
+    "type-at-pos",
+    "--expand-type-aliases",
+    "--expand-json-output",
+    filename,
+    line,
+    column + 1
+  ];
+  return JSON.parse(execa.sync(flow, args).stdout);
 }
 
-const plugins = ["flow", "flowComments"];
+const defaultPlugins = ["flow", "flowComments"];
 
 function convertType(type: string, plugins: string[]): string {
   if (type === "any (explicit)") return "any";
@@ -61,13 +68,47 @@ function convertType(type: string, plugins: string[]): string {
   return type;
 }
 
+function skip(path, callback) {
+  if (t.isProgram(path.node)) return;
+  if (t.isImportDeclaration(path.node)) return;
+  if (t.isImportSpecifier(path.node)) return;
+  if (t.isTypeAlias(path.node)) return;
+  if (t.isTypeAlias(path.parent)) return;
+  if (t.isGenericTypeAnnotation(path.parent)) return;
+  if (t.isGenericTypeAnnotation(path.node)) return;
+  if (t.isNewExpression(path.node)) return;
+  if (t.isUnaryExpression(path.node)) return;
+  if (t.isTypeParameterDeclaration(path.node)) return;
+  if (t.isArrowFunctionExpression(path.node)) return;
+  if (t.isVariableDeclaration(path.node)) return;
+  if (t.isExportNamedDeclaration(path.node)) return;
+
+  // Skip literals
+  if (t.isLiteral(path.node)) return;
+
+  // Skip statements
+  if (t.isStatement(path.node)) return;
+
+  // Skip all type annotations
+  if (t.isTypeAnnotation(path.node)) return;
+  if (t.isMixedTypeAnnotation(path.node)) return;
+  if (t.isBooleanLiteralTypeAnnotation(path.node)) return;
+  if (t.isStringTypeAnnotation(path.node)) return;
+  if (t.isStringLiteralTypeAnnotation(path.node)) return;
+  if (t.isNullLiteralTypeAnnotation(path.node)) return;
+  if (t.isUnionTypeAnnotation(path.node)) return;
+  if (t.isVoidTypeAnnotation(path.node)) return;
+
+  callback();
+}
+
 export function parseFileSync(
   filename: string,
   {
     sourceType = "module",
-    plugins = plugins,
+    plugins = defaultPlugins,
     ...options
-  }: { [key: string]: any } = { sourceType: "module", plugins }
+  }: { [key: string]: any } = { sourceType: "module", plugins: defaultPlugins }
 ) {
   const code = fs.readFileSync(filename, { encoding: "utf8" });
   const ast = parser.parse(code, {
@@ -76,20 +117,19 @@ export function parseFileSync(
     sourceType
   });
 
-  if (flowVersion === null) {
-    throw new Error("flow-bin must be installed");
-  }
-
   function assignType(path) {
     const symbol = getTypeAtPosSync(path.node.loc.start, filename);
     const type = convertType(symbol.type, plugins);
-    //console.log(path.node.type, symbol.type);
     if (type !== "(unknown)") {
+      //console.log(path.node.type, symbol.type, path.node);
       try {
         symbol.type = template.ast(`type S = ${type}`, {
           plugins
         }).right;
         path.node.symbol = symbol;
+        if (t.isIdentifier(path.node)) {
+          path.parent.symbol = symbol;
+        }
       } catch (err) {
         console.error("Error on ", path.node.loc.start, path.node, path.parent);
         console.error(err);
@@ -99,16 +139,7 @@ export function parseFileSync(
 
   traverse(ast, {
     enter(path) {
-      if (t.isTypeAlias(path.parent)) {
-        return;
-      }
-      if (t.isGenericTypeAnnotation(path.parent)) {
-        return;
-      }
-      if (t.isGenericTypeAnnotation(path.node)) {
-        return;
-      }
-      assignType(path);
+      skip(path, () => assignType(path));
     }
   });
 
@@ -119,9 +150,9 @@ export async function parseFile(
   filename: string,
   {
     sourceType = "module",
-    plugins = plugins,
+    plugins = defaultPlugins,
     ...options
-  }: { [key: string]: any } = { sourceType: "module", plugins }
+  }: { [key: string]: any } = { sourceType: "module", plugins: defaultPlugins }
 ) {
   const code = await fs.readFile(filename, { encoding: "utf8" });
   const ast = parser.parse(code, {
@@ -129,10 +160,6 @@ export async function parseFile(
     sourceType,
     plugins
   });
-
-  if (flowVersion === null) {
-    throw new Error("flow-bin must be installed");
-  }
 
   let promises = [];
 
@@ -146,6 +173,9 @@ export async function parseFile(
           plugins
         }).right;
         path.node.symbol = symbol;
+        if (t.isIdentifier(path.node)) {
+          path.parent.symbol = symbol;
+        }
       } catch (err) {
         console.error("Error on ", path.node.loc.start, path.node, path.parent);
         console.error(err);
@@ -155,16 +185,9 @@ export async function parseFile(
 
   traverse(ast, {
     enter(path) {
-      if (t.isTypeAlias(path.parent)) {
-        return;
-      }
-      if (t.isGenericTypeAnnotation(path.parent)) {
-        return;
-      }
-      if (t.isGenericTypeAnnotation(path.node)) {
-        return;
-      }
-      promises.push(assignType(path));
+      skip(path, () => {
+        promises.push(assignType(path));
+      });
     }
   });
 
